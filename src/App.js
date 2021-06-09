fac985f1adbce324ba5abe074f8617b8143b2999
Akash <akashshekara1212@gmail.com>
2021-06-08 22:47:39 -0500

    added mux video

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Mux from '@mux/mux-node';
+const { Video } = new Mux('7441578e-a733-4a1b-a32f-2b7ccc388c23', 'wUjBnDqjC/FnL0lyeAzZNujm4AcwT7gBXJtpcm/p6SgOT5cIUSmX8VNts62fA54DjJ3cUcaKsqV');
 
-function App() {
+async function createVideo() {
+  const assets = await Video.Uploads.create({
+    input: 'https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4',
+    cors_origin: '*',
+  });
+  return assets;
+}
+
+
+function App(){
+  const something = createVideo();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {console.log(something)}
     </div>
-  );
+  )
 }
 
 export default App;
